948f950f0ba9fd9df2932e81f595d59935294e8b
Omar Alawneh <omaralawneh01@gmail.com>
2025-10-13 19:08:33 +0300

    a little changes on home section and in the social  media icons

--- src/home.jsx
+++ src/home.jsx
@@ -1,47 +1,27 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaLinkedin, FaGithub , FaInstagram ,FaXTwitter  } from 'react-icons/fa';
-  
+import { FaFacebook, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 
-
-function home() {
-   return (
-    <>
-     <section className="main">
-      <h2>
-        hi I'm 'Omar'
-        <br />
-        <span>Web Developer {"{Front End}"}</span>
-        <br />
-        <span>
-          <h3>Whether you're a business owner needing a landing page,<br />
-             a startup looking for a clean web app UI,<br />
-              or a team needing a developer to bring your frontend vision to life <br />
-              I can help.</h3>
-        </span>
-        <span>
-          <a href="#project" className="main-btn">
-            take a look
-          </a>
-          <div className="social-icons">
-            <h5>my social media</h5>
-            <a href="https://github.com/omaralawneh01">
-              <i><FaGithub/></i>
-            </a>
-            <a href="https://www.linkedin.com/in/omar-alawneh-1a532124b/">
-              <i><FaLinkedin/></i>
-            </a>
-            <a href="https://www.instagram.com/omar_alawneh01/">
-              <i><FaInstagram/></i>
-            </a>
-            <a href="https://www.facebook.com/omar.alawneh.549">
-              <i><FaFacebook/></i>
-            </a>
-          </div>
-        </span>
+function Home() {
+  return (
+    <section className="main">
+      <h2>Hey there 👋 I’m Omar Alawneh <br />
+      <span>a Full-Stack Web Developer</span>
+      <h3>
+        Who enjoys building everything from dynamic frontends to powerful backends.
+      </h3>
       </h2>
+      <a href="#project" className="main-btn">
+        Take a look
+      </a>
+      <div className="social-icons">
+        <h4>My social media</h4>
+        <a href="https://github.com/omaralawneh01"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/omar-alawneh-1a532124b/" ><FaLinkedin /></a>
+        <a href="https://www.instagram.com/omar_alawneh01/"><FaInstagram /></a>
+        <a href="https://www.facebook.com/omar.alawneh.549"><FaFacebook /></a>
+      </div>
     </section>
-     </>
-   
   );
 }
-export default home;
+
+export default Home;
